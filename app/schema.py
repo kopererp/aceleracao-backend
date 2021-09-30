eb518ec734925c5293f1cad13dfdeaf8e92f5ae5
Willian Morais <williandmorais@gmail.com>
2021-09-30 10:35:36 -0300

    feat: add the hello example mutation and query

--- app/schema.py
+++ app/schema.py
@@ -1,9 +1,23 @@
 import graphene
 
 
-class Query:
-    pass
+class Hello(graphene.ObjectType):
+    message = graphene.String(required=True, description="Welcome message")
+
+
+class SayHello(graphene.Mutation):
+    class Arguments:
+        your_name = graphene.String(required=True, description="Your name")
+
+    Output = Hello
+
+    def mutate(self, info, your_name: str) -> Hello:
+        return Hello(message=f"Hello {your_name}")
+
+
+class Query(graphene.ObjectType):
+    hello = graphene.String(default_value="Hello")
 
 
 class Mutation(graphene.ObjectType):
-    pass
+    say_hello = SayHello.Field()
