afbc7ac414724c944f00ddc3ed07b077083a04c2
Willian Morais <williandmorais@gmail.com>
2021-09-30 14:12:04 -0300

    feat: implement the TwitterMessageUpdateMutation

--- app/twitter_example/mutations.py
+++ app/twitter_example/mutations.py
@@ -1,13 +1,18 @@
-import graphene
+from typing import Optional
+from uuid import UUID
 
+import graphene
 from app.documents import TwitterMessage as TwitterMessageDocument
+
 from .types import TwitterMessage
 
 
-class TwitterMessageMutation(graphene.Mutation):
+class TwitterMessageCreateMutation(graphene.Mutation):
     class Arguments:
-        title = graphene.String(description="Title of the message")
-        message = graphene.String(description="Description of the message")
+        title = graphene.String(required=True, description="Title of the message")
+        message = graphene.String(
+            required=True, description="Description of the message"
+        )
 
     Output = TwitterMessage
 
@@ -16,3 +21,31 @@ class TwitterMessageMutation(graphene.Mutation):
         twitter_message.save()
 
         return twitter_message
+
+
+class TwitterMessageUpdateMutation(graphene.Mutation):
+    class Arguments:
+        message_id = graphene.ID(
+            required=True, description="Identification of the message"
+        )
+        title = graphene.String(required=False, description="Title of the message")
+        message = graphene.String(
+            required=False, description="Description of the message"
+        )
+
+    Output = TwitterMessage
+
+    def mutate(self, info, message_id: UUID, **kwargs) -> TwitterMessageDocument:
+        try:
+            twitter_message = TwitterMessageDocument.objects.get(message_id=message_id)
+
+            for key, value in kwargs.items():
+                if not value:
+                    continue
+
+                setattr(twitter_message, key, value)
+
+            twitter_message.save()
+            return twitter_message
+        except TwitterMessageDocument.DoesNotExist:
+            raise Exception("not found")
