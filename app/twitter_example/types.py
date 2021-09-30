8574d6e91b103ca9f5a5d1587db45b7ff7275c1b
Willian Morais <williandmorais@gmail.com>
2021-09-30 14:25:08 -0300

    feat: implement the SuccessDelete type

--- app/twitter_example/types.py
+++ app/twitter_example/types.py
@@ -1,3 +1,5 @@
+import graphene
+
 from app.documents import TwitterMessage as TwitterMessageDocument
 from graphene.relay import Node
 from graphene_mongo import MongoengineObjectType
@@ -7,3 +9,9 @@ class TwitterMessage(MongoengineObjectType):
     class Meta:
         model = TwitterMessageDocument
         interfaces = (Node,)
+
+
+class SuccessDelete(graphene.ObjectType):
+    success = graphene.Boolean(
+        required=True, description="Indicate success on the operation"
+    )
