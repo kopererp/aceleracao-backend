4be6d625a48bfdde3fbe2f6f1608e648a5942b9f
Willian Morais <williandmorais@gmail.com>
2021-09-30 14:12:28 -0300

    feat: implement the listing of twitter messages query

--- app/twitter_example/queries.py
+++ app/twitter_example/queries.py
@@ -1,3 +1,5 @@
+from typing import List
+
 import graphene
 from app.documents import TwitterMessage as TwitterMessageDocument
 
@@ -9,9 +11,13 @@ class TwitterMessageQuery(graphene.ObjectType):
         TwitterMessage,
         id=graphene.ID(required=True, description="Message's identification"),
     )
+    twitter_messages = graphene.List(TwitterMessage)
 
-    def resolve_twitter_message(root, info, id: graphene.ID) -> TwitterMessage:
+    def resolve_twitter_message(root, info, id: graphene.ID) -> TwitterMessageDocument:
         try:
             return TwitterMessageDocument.objects.get(message_id=id)
         except TwitterMessageDocument.DoesNotExist:
             raise Exception("not found")
+
+    def resolve_twitter_messages(root, info) -> List[TwitterMessageDocument]:
+        return TwitterMessageDocument.objects.all()
