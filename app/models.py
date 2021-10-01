9c0bf7e17947ce51766eadb582e155884b7df791
Willian Morais <williandmorais@gmail.com>
2021-10-01 14:48:50 -0300

    feat: implement the User model

---
+++ app/models.py
@@ -0,0 +1,24 @@
+import sqlalchemy as sa
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+from app.settings import settings
+
+engine = create_engine(settings.POSTGRES_URI)
+Session = sessionmaker(autocommit=False, bind=engine)
+ScopedSession = scoped_session(Session)
+
+Base = declarative_base()
+Base.query = ScopedSession.query_property()
+
+
+class User(Base):
+    __tablename__ = "user"
+
+    id = sa.Column(sa.UUID, primary_key=True)
+    name = sa.Column(sa.Text)
+    email = sa.Column(sa.Text)
+
+
+Base.metadata.create_all(engine)
