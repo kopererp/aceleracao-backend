11bce18f01579f7644476b22f2417936005973d2
Willian Morais <williandmorais@gmail.com>
2021-10-01 14:24:07 -0300

    feat: implement the TwitterMessageCreateValidator

--- app/twitter_example/validators.py
+++ app/twitter_example/validators.py
@@ -3,6 +3,11 @@ from typing import Optional
 import pydantic
 
 
-class TwitterMessageValidator(pydantic.BaseModel):
+class TwitterMessageCreateValidator(pydantic.BaseModel):
+    title: pydantic.constr(strip_whitespace=True, max_length=80)
+    message: pydantic.constr(strip_whitespace=True, max_length=280)
+
+
+class TwitterMessageUpdateValidator(pydantic.BaseModel):
     title: Optional[pydantic.constr(strip_whitespace=True, max_length=80)]
     message: Optional[pydantic.constr(strip_whitespace=True, max_length=280)]
