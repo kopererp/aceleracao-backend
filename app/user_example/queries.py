7a95908ec8b2166961037f20f2d6f8f1978aa57e
Willian Morais <williandmorais@gmail.com>
2021-10-01 17:25:54 -0300

    feat: implemen the query user

--- app/user_example/queries.py
+++ app/user_example/queries.py
@@ -1,8 +1,21 @@
+from uuid import UUID
+
 import graphene
+from sqlalchemy.orm.exc import NoResultFound
 from graphene_sqlalchemy import SQLAlchemyConnectionField
 
+from app.models import User as UserModel
 from .types import User
 
 
 class UserQuery(graphene.ObjectType):
-    users = SQLAlchemyConnectionField(User)
+    user = graphene.Field(
+        User, user_id=graphene.ID(required=True, description="The user identification"), description="Return an user"
+    )
+    users = SQLAlchemyConnectionField(User, description="Return a list of users")
+
+    def resolve_user(root, info, user_id: UUID):
+        try:
+            return UserModel.query.filter_by(user_id=user_id).one()
+        except NoResultFound:
+            raise Exception("not found")
