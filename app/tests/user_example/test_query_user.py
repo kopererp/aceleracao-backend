fb1a55ba232a161aa34b4f9ad5dc44f2d7174d33
Willian Morais <williandmorais@gmail.com>
2021-10-01 17:26:23 -0300

    test: add test cases for the query user

---
+++ app/tests/user_example/test_query_user.py
@@ -0,0 +1,37 @@
+from app.tests.factories import UserFactory
+
+
+def test_should_return_the_user(session, client):
+    user = UserFactory()
+
+    query = f"""
+        query {{
+            user(userId: "{user.user_id}") {{
+                userId
+                name
+                email
+            }}
+        }}
+    """
+    response = client.execute(query)
+    data = response["data"]
+
+    assert data == {
+        "user": {"userId": "8631d2f7-8ecf-4102-a3e3-42c244a82300", "name": "Name 0", "email": "name+0@user.com"}
+    }
+
+
+def test_should_return_an_error_when_there_is_not_user(session, client):
+    query = """
+        query {
+            user(userId: "8631d2f7-8ecf-4102-a3e3-42c244a82300") {
+                userId
+                name
+                email
+            }
+        }
+    """
+    response = client.execute(query)
+    errors = response["errors"]
+
+    assert errors == [{"message": "not found", "locations": [{"line": 3, "column": 13}], "path": ["user"]}]
