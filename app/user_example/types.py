6e46aec74c70589f9fb20bd7e17c527d18b0f49c
Willian Morais <williandmorais@gmail.com>
2021-10-01 17:12:05 -0300

    feat: add the relay.Node to the User type

--- app/user_example/types.py
+++ app/user_example/types.py
@@ -1,3 +1,4 @@
+from graphene import relay
 from graphene_sqlalchemy import SQLAlchemyObjectType
 
 from app.models import User as UserModel
@@ -6,3 +7,4 @@ from app.models import User as UserModel
 class User(SQLAlchemyObjectType):
     class Meta:
         model = UserModel
+        interfaces = (relay.Node,)
