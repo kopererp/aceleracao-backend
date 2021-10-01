a976dc5b64caee955dd3f1ebefe7cde32e71a71a
Willian Morais <williandmorais@gmail.com>
2021-10-01 17:51:54 -0300

    feat: expose the UserCreateMutation to the schema

--- app/schema.py
+++ app/schema.py
@@ -8,6 +8,7 @@ from .twitter_example.mutations import (
 )
 from .twitter_example.queries import TwitterMessageQuery
 from .user_example.queries import UserQuery
+from .user_example.mutations import UserCreateMutation
 
 
 class Query(TwitterMessageQuery, UserQuery):
@@ -18,3 +19,5 @@ class Mutation(graphene.ObjectType):
     create_twitter_message = TwitterMessageCreateMutation.Field()
     update_twitter_message = TwitterMessageUpdateMutation.Field()
     delete_twitter_message = TwitterMessageDeleteMutation.Field()
+
+    create_user = UserCreateMutation.Field()
