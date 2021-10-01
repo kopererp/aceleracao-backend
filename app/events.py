79b49320302a720831833cd7412b73d791014305
Willian Morais <williandmorais@gmail.com>
2021-10-01 17:05:07 -0300

    feat: add the Base.create_all at the application startup

--- app/events.py
+++ app/events.py
@@ -1,10 +1,12 @@
 from mongoengine import connect, disconnect
 
+from app.models import Base, engine
 from app.settings import settings
 
 
 def on_application_startup():
     connect(host=settings.MONGO_URI)
+    Base.metadata.create_all(engine)
 
 
 def on_application_shutdown():
