765ca1e7db967da5f3fb3b605086aec11035aea2
Willian Morais <williandmorais@gmail.com>
2021-10-01 14:24:30 -0300

    feat: add the validator to TwitterMessageCreateMutation

--- app/twitter_example/mutations.py
+++ app/twitter_example/mutations.py
@@ -5,7 +5,7 @@ from app.documents import TwitterMessage as TwitterMessageDocument
 from pydantic import ValidationError
 
 from .types import SuccessDelete, TwitterMessage
-from .validators import TwitterMessageValidator
+from .validators import TwitterMessageCreateValidator, TwitterMessageUpdateValidator
 
 
 class TwitterMessageCreateMutation(graphene.Mutation):
@@ -18,10 +18,15 @@ class TwitterMessageCreateMutation(graphene.Mutation):
     Output = TwitterMessage
 
     def mutate(self, info, title: str, message: str) -> TwitterMessageDocument:
-        twitter_message = TwitterMessageDocument(title=title, message=message)
-        twitter_message.save()
+        try:
+            TwitterMessageCreateValidator(title=title, message=message)
+            twitter_message = TwitterMessageDocument(title=title, message=message)
+            twitter_message.save()
 
-        return twitter_message
+            return twitter_message
+        except ValidationError as e:
+            error = e.errors()[0]["msg"]
+            raise Exception(error)
 
 
 class TwitterMessageUpdateMutation(graphene.Mutation):
@@ -38,7 +43,7 @@ class TwitterMessageUpdateMutation(graphene.Mutation):
 
     def mutate(self, info, message_id: UUID, **kwargs) -> TwitterMessageDocument:
         try:
-            TwitterMessageValidator(**kwargs)
+            TwitterMessageUpdateValidator(**kwargs)
             twitter_message = TwitterMessageDocument.objects.get(message_id=message_id)
 
             for key, value in kwargs.items():
