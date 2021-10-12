bdc7568e71ffacef689fc30d86ef583da78c8bde
Willian Morais <williandmorais@gmail.com>
2021-10-12 18:36:25 -0300

    feat: implement the UserUpdateMutation

--- app/user_example/mutations.py
+++ app/user_example/mutations.py
@@ -1,9 +1,14 @@
+from typing import Optional
+from uuid import UUID
+
 import graphene
+from pydantic import ValidationError
+from sqlalchemy.orm.exc import NoResultFound
 
 from app.models import User as UserModel, get_session
 
 from .types import User
-from .validators import UserCreateValidator
+from .validators import UserCreateValidator, UserUpdateValidator
 
 
 class UserCreateMutation(graphene.Mutation):
@@ -14,13 +19,53 @@ class UserCreateMutation(graphene.Mutation):
     Output = User
 
     def mutate(root, info, name: str, email: str) -> UserModel:
-        UserCreateValidator(name=name, email=email)
+        try:
+            UserCreateValidator(name=name, email=email)
 
-        user = UserModel(name=name, email=email)
+            user = UserModel(name=name, email=email)
 
-        with get_session() as session:
-            session.add(user)
-            session.commit()
-            session.refresh(user)
+            with get_session() as session:
+                session.add(user)
+                session.commit()
+                session.refresh(user)
 
-        return user
+            return user
+
+        except ValidationError as e:
+            error = e.errors()[0]["msg"]
+            raise Exception(error)
+
+
+class UserUpdateMutation(graphene.Mutation):
+    class Arguments:
+        user_id = graphene.ID(required=True, description="The user identification")
+        name = graphene.String(required=False, description="The user name")
+        email = graphene.String(required=False, description="The user e-mail")
+
+    Output = User
+
+    def mutate(root, info, user_id: UUID, **kwargs) -> UserModel:
+        try:
+            UserUpdateValidator(**kwargs)
+
+            with get_session() as session:
+                user = UserModel.query.filter_by(user_id=user_id).one()
+
+                for key, value in kwargs.items():
+                    if not value:
+                        continue
+
+                    setattr(user, key, value)
+
+                session.add(user)
+                session.commit()
+                session.refresh(user)
+
+            return user
+
+        except NoResultFound:
+            raise Exception("not found")
+
+        except ValidationError as e:
+            error = e.errors()[0]["msg"]
+            raise Exception(error)
