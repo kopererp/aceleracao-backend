fc4f24f58fc062c79f667056d03bed2da585e8f6
Willian Morais <williandmorais@gmail.com>
2021-10-12 18:38:09 -0300

    feat: implement the UserUpdateValidator

--- app/user_example/validators.py
+++ app/user_example/validators.py
@@ -1,6 +1,13 @@
+from typing import Optional
+
 import pydantic
 
 
 class UserCreateValidator(pydantic.BaseModel):
-    name: str
+    name: pydantic.constr(strip_whitespace=True, min_length=1)
     email: pydantic.EmailStr
+
+
+class UserUpdateValidator(pydantic.BaseModel):
+    name: Optional[pydantic.constr(strip_whitespace=True, min_length=1)]
+    email: Optional[pydantic.EmailStr]
