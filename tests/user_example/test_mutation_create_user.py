f56af0cff2f72230f6ba547ca956cb61a873400a
Willian Morais <williandmorais@gmail.com>
2021-10-12 18:38:31 -0300

    test: add validation test cases for createUser mutation

--- tests/user_example/test_mutation_create_user.py
+++ tests/user_example/test_mutation_create_user.py
@@ -3,7 +3,7 @@ from unittest.mock import patch
 
 
 @patch("uuid.uuid4", return_value=uuid.UUID("5a9ee8c5-ed55-4239-9758-5422292dafd0"))
-def test_should_create_an_user(uuid_mock, client, set_scoped_session):
+def test_should_create_an_user(uuid_mock, client):
     query = """
         mutation {
             createUser(name: "User", email: "user@user.com") {
@@ -19,3 +19,69 @@ def test_should_create_an_user(uuid_mock, client, set_scoped_session):
     assert data == {
         "createUser": {"userId": "5a9ee8c5-ed55-4239-9758-5422292dafd0", "name": "User", "email": "user@user.com"}
     }
+
+
+def test_should_return_an_error_on_empty_name(client):
+    query = """
+        mutation {
+            createUser(name: "", email: "user@user.com") {
+                userId
+                name
+                email
+            }
+        }
+    """
+    response = client.execute(query)
+    errors = response["errors"]
+
+    assert errors == [
+        {
+            "message": "ensure this value has at least 1 characters",
+            "locations": [{"line": 3, "column": 13}],
+            "path": ["createUser"],
+        }
+    ]
+
+
+def test_should_return_an_error_on_invalid_email(client):
+    query = """
+        mutation {
+            createUser(name: "User", email: "invalid@email") {
+                userId
+                name
+                email
+            }
+        }
+    """
+    response = client.execute(query)
+    errors = response["errors"]
+
+    assert errors == [
+        {
+            "message": "value is not a valid email address",
+            "locations": [{"line": 3, "column": 13}],
+            "path": ["createUser"],
+        }
+    ]
+
+
+def test_should_return_an_error_on_empty_email(client):
+    query = """
+        mutation {
+            createUser(name: "User", email: "") {
+                userId
+                name
+                email
+            }
+        }
+    """
+    response = client.execute(query)
+    errors = response["errors"]
+
+    assert errors == [
+        {
+            "message": "value is not a valid email address",
+            "locations": [{"line": 3, "column": 13}],
+            "path": ["createUser"],
+        }
+    ]
