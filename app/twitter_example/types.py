ec1af7516d995f6cb603f87fb300655a9a12299e
Willian Morais <williandmorais@gmail.com>
2021-10-13 07:49:01 -0300

    refactor: move the SuccessDelete delete

--- app/twitter_example/types.py
+++ app/twitter_example/types.py
@@ -1,15 +1,10 @@
-import graphene
-
-from app.documents import TwitterMessage as TwitterMessageDocument
 from graphene.relay import Node
 from graphene_mongo import MongoengineObjectType
+
+from app.documents import TwitterMessage as TwitterMessageDocument
 
 
 class TwitterMessage(MongoengineObjectType):
     class Meta:
         model = TwitterMessageDocument
         interfaces = (Node,)
-
-
-class SuccessDelete(graphene.ObjectType):
-    success = graphene.Boolean(required=True, description="Indicate success on the operation")
